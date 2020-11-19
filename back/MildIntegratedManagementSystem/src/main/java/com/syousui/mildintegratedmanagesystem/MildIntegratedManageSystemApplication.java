ca10abf8c12337d3be295bd91b147ff8fd905b9a
syouSui <justTouSan@gmail.com>
2020-11-19 19:56:13 +0800

    添加@srvice支持.

--- back/MildIntegratedManagementSystem/src/main/java/com/syousui/mildintegratedmanagesystem/MildIntegratedManageSystemApplication.java
+++ back/MildIntegratedManagementSystem/src/main/java/com/syousui/mildintegratedmanagesystem/MildIntegratedManageSystemApplication.java
@@ -3,11 +3,13 @@ package com.syousui.mildintegratedmanagesystem;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @MapperScan ( "com.syousui.mildintegratedmanagesystem.mapper" )
 @EnableTransactionManagement ()
+@ComponentScan ( "com.syousui.mildintegratedmanagesystem.service" )
 public class MildIntegratedManageSystemApplication {
 
     public static void main ( String[] args ) {
