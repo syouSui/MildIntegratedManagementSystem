e92b6a7cb40f74ae8822f6b1f6c939751d86df53
syouSui <justTouSan@gmail.com>
2020-11-20 22:10:42 +0800

    添加Swagger3框架用于API文档生成, 部分参数有显示问题, 仅作参考.访问: http://localhost:8888/swagger-ui/index.html

--- back/MildIntegratedManagementSystem/src/main/java/com/syousui/mildintegratedmanagesystem/MildIntegratedManageSystemApplication.java
+++ back/MildIntegratedManagementSystem/src/main/java/com/syousui/mildintegratedmanagesystem/MildIntegratedManageSystemApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-import springfox.documentation.oas.annotations.EnableOpenApi;
 
 @SpringBootApplication
 @MapperScan ( "com.syousui.mildintegratedmanagesystem.mapper" )
